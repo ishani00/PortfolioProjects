404f987ac224639bef141754caede7088e824c08
ishani00 <117313740+ishani00@users.noreply.github.com>
2023-11-25 02:24:09 +0530

    Add files via upload

---
+++ COVID Portfolio Project.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [Project1].[dbo].[PercentagePopulationVaccinated]
